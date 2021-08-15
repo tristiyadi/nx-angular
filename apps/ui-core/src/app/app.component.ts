f70959ffd4a80189fb177afd460e5e6142a6cd94
ekotristiyadi1 <eko@paques.id>
2021-08-15 08:35:24 +0700

    initial commit

---
+++ apps/ui-core/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ui-core-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ui-core';
+}
